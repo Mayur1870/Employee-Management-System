e0af7a0eacf5d65f73e2bec74a3552ef455210cb
Mayur Savaisarje <54172286+Mayur1870@users.noreply.github.com>
2024-11-07 14:06:51 +0530

    EmployeeData.java
    Used Lombok to reduce boilerplate code.

--- EmployeeData.java
+++ EmployeeData.java
@@ -1,90 +1,36 @@
-package com.Employee;
+package com.employeemgmt.employeemanagementsystem;
 
-import java.util.ArrayList;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-public final class EmployeeData {
-	
-	private int empID;
-	private String empName;
-	private int age;
-	private double salary;
-	private String designation;
-	private long contactNo;
-	
-//	public EmployeeData() {
-//		
-//	}
-	
-	public EmployeeData(int empID, String empName, int age, double salary, String designation, long contactNo) {
-        this.empID = empID;
-        this.empName = empName;
-        this.age = age;
-        this.salary = salary;
-        this.designation = designation;
-        this.contactNo = contactNo;
+@Getter
+@Setter
+@Entity
+public class EmployeeData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empID;
+    private String empName;
+    private int age;
+    private double salary;
+    private String designation;
+    private long contactNo;
+
+    @Override
+    public String toString() {
+        return "EmployeeData{" +
+                "empID=" + empID +
+                ", empName='" + empName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", designation='" + designation + '\'' +
+                ", contactNo=" + contactNo +
+                '}';
     }
 
-	public int getEmpID() {
-		return empID;
-	}
-
-//	public void setEmpID(int empID) {
-//		this.empID = empID;
-//	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-//	public void setEmpName(String empName) {
-//		this.empName = empName;
-//	}
-
-	public int getage() {
-		return age;
-	}
-
-//	public void setage(int age) {
-//		this.age = age;
-//	}
-
-	public double getSalary() {
-		return salary;
-	}
-
-//	public void setSalary(double salary) {
-//		this.salary = salary;
-//	}
-
-	public String getDesignation() {
-		return designation;
-	}
-
-//	public void setDesignation(String designation) {
-//		this.designation = designation;
-//	}
-
-	public long getContactNo() {
-		return contactNo;
-	}
-
-//	public void setContactNo(long contactNo) {
-//		this.contactNo = contactNo;
-//	}
-
-	@Override
-	public String toString() {
-		return "[empID = " + empID + ", empName = " + empName + ", age = " + age + ", salary = " + salary
-				+ ", designation = " + designation + ", contactNo = " + contactNo + "]";
-	}
-	
-	public boolean checkRetirement() {
-		if(age > 65) {
-			return true;
-		}
-		else {
-		return false;
-		}
-	}
-	
 }
