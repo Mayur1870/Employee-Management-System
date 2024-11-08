33b5829f8fbb26ef8f5a7fd4bd5374b9eb8619d6
Mayur Savaisarje <54172286+Mayur1870@users.noreply.github.com>
2024-11-08 21:28:27 +0530

    EmployeeRepository.java

--- EmployeeRepository.java
+++ EmployeeRepository.java
@@ -1,7 +1,19 @@
-package com.employeemgmt.employeemanagementsystem;
+package com.employeemgmt.employeemanagementsystem.repo;
 
+import com.employeemgmt.employeemanagementsystem.entity.EmployeeData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeData, Integer> {
 
+    @Query("SELECT e FROM EmployeeData e WHERE e.salary > ?1 ORDER BY e.salary ASC")
+    List<EmployeeData> findBySalaryGreaterThan(double salary);
+
+    @Query("SELECT e FROM EmployeeData e WHERE e.salary = (SELECT MAX(e2.salary) FROM EmployeeData e2)")
+    List<EmployeeData> findEmployeesWithMaxSalary();
+
+    @Query("SELECT e from EmployeeData e where e.age > ?1")
+    List<EmployeeData> findEmployeesWithAge(int age);
 }
